43796ab63f4ea76e2426079bf4d6f86fd5c54f70
Kauna Mohammed <kaunamohammed@gmail.com>
2020-06-03 13:09:50 +0100

    Update CoordinatorTests.swift
    added tests

--- Tests/CoordinatorTests/CoordinatorTests.swift
+++ Tests/CoordinatorTests/CoordinatorTests.swift
@@ -2,14 +2,64 @@ import XCTest
 @testable import Coordinator
 
 final class CoordinatorTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Coordinator().text, "Hello, World!")
+    class CoordinatorA: BaseChildCoordinator<UIViewController> {}
+    
+    class CoordinatorB: Coordinator {
+        func start() {
+            print("MockB started")
+        }
+    }
+    
+    func testChildCoordinatorAdded() {
+        let coordinatorA = CoordinatorA()
+        let coordinatorB = CoordinatorB()
+        coordinatorA.add(coordinator: coordinatorB, key: "coordinator_b")
+
+        XCTAssertTrue(!coordinatorA.children.isEmpty)
+    }
+    
+    func testChildKeyAutomaticAssigning() {
+        let coordinatorA = CoordinatorA()
+        let coordinatorB = CoordinatorB()
+        let coordinatorC = CoordinatorB()
+        let coordinatorD = CoordinatorB()
+        let coordinatorE = CoordinatorB()
+        let coordinatorF = CoordinatorB()
+
+        coordinatorA.add(coordinator: coordinatorB)
+        coordinatorA.add(coordinator: coordinatorC)
+        coordinatorA.add(coordinator: coordinatorD)
+        coordinatorA.add(coordinator: coordinatorE)
+        coordinatorA.add(coordinator: coordinatorF)
+
+        XCTAssertTrue(coordinatorA.children["4"]! === coordinatorF)
     }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    func testOnlyOneInstanceOfChildCoordinator() {
+        let coordinatorA = CoordinatorA()
+        let coordinatorB = CoordinatorB()
+        coordinatorA.add(coordinator: coordinatorB, key: "coordinator_b")
+        coordinatorA.add(coordinator: coordinatorB, key: "coordinator_b")
+        coordinatorA.add(coordinator: coordinatorB, key: "coordinator_b")
+        
+        XCTAssertTrue(coordinatorA.children.values.count == 1)
+    }
+    
+    func testChildCordinatorRemoved() {
+        let coordinatorA = CoordinatorA()
+        let coordinatorB = CoordinatorB()
+        coordinatorA.add(coordinator: coordinatorB, key: "coordinatorB")
+        coordinatorA.remove(coordinator: coordinatorB)
+        
+        XCTAssertTrue(coordinatorA.children.isEmpty)
+    }
+    
+    func testRemoveAllChildCoordinators() {
+        let coordinatorA = CoordinatorA()
+        let children = ["child1": CoordinatorB(), "child2": CoordinatorB(), "child3": CoordinatorB()]
+        children.forEach { coordinatorA.add(coordinator: $0.value, key: $0.key) }
+        coordinatorA.removeAll()
+        
+        XCTAssertTrue(coordinatorA.children.isEmpty)
+    }
 }
